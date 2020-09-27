f1fdd282999affc350fb384466e1bb006d0ca46d
ryan <ryanpauley@gmail.com>
2020-09-27 18:49:02 +1000

    Handles bad argv cases/help message

--- lib/resistor/cli_parser.ex
+++ lib/resistor/cli_parser.ex
@@ -3,7 +3,11 @@ defmodule Resistor.CliParser do
     options = [switches: [file: :string],aliases: [f: :file]]
     {_, bands, _} = OptionParser.parse(args, options)
 
-    Resistor.Calc.compute(bands)
-    |> IO.puts
+    if length(bands) in [4, 5] do
+      Resistor.Calc.compute(bands)
+      |> IO.puts
+    else
+      IO.puts "Incorrect band combination. Handles 4 & 5 band resistors.\nUsage:\n\t./resistor red green blue gold"
+    end
   end
 end
